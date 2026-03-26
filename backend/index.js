b7bff1f9f8e93b6f7a919d70d5d3b9066b98ab49
rainy <rainy.chen@mail.utoronto.ca>
2026-03-26 17:09:52 -0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,12 @@ import express from "express";
 import routes from "./routes.js";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 
@@ -12,5 +16,11 @@ const app = express();
 app.use(cors({ origin: FRONTEND_URL }));
 app.use(express.json());
 app.use('', routes);
+
+const distPath = path.join(__dirname, "../frontend/dist");
+app.use(express.static(distPath));
+app.get("*", (_req, res) => {
+    res.sendFile(path.join(distPath, "index.html"));
+});
 
 export default app;
